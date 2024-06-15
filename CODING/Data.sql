4ee6706e4202aece67b4764b5b0fba9e900eae65
BuiKhanh0130 <144783579+BuiKhanh0130@users.noreply.github.com>
2024-06-15 08:42:14 +0700

    Add files via upload

---
+++ CODING/Data.sql
@@ -0,0 +1,144 @@
+﻿
+/*INSERT USERS*/
+INSERT INTO Users (Id, FullName, Gender, Email, PhoneNumber, isActive, RefreshToken, DateExpireRefreshToken, RoleId, UserName, NormalizedUserName, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount) 
+VALUES ('c93968ff-4a02-4dd0-9e91-ca96bf57b694', 'Alice', 0, 'alice123@gmail.com', 097873483, 1, null, null, null, 'tutor1', 'TUTOR1', null, 0, 'AQAAAAIAAYagAAAAELUtBsYUk4h5Jc2zc4etb6WCFVGK8R9dSBgeKvl9yBPkaFLsWUIueCtzO+59FxHqWA==', 'LYHISYT3ZII727ANP2QLAT4CE2HIWQL5', '1b65832c-7007-4c22-b797-7bb9f87e56b1', 0, 0, null, 1, 0);
+INSERT INTO Users (Id, FullName, Gender, Email, PhoneNumber, isActive, RefreshToken, DateExpireRefreshToken, RoleId, UserName, NormalizedUserName, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount) 
+VALUES ('33bbad25-0741-4588-b596-6504045778da', 'John', 1, 'john123@gmail.com', 097873483, 1, null, null, null, 'tutor2', 'TUTOR2', null, 0, 'AQAAAAIAAYagAAAAEHhwhC1EOqoX/6Kc/sHGK3uqMUM6ydjg4tJt3OSWxT4OXI35rODaffGd36VqsPJS1A==', 'M4QGKWUYE6ETZBIZPO4PLAQGFGOGBHOX', 'ffe8b51b-7723-429f-b3d8-0267f23f2852', 0, 0, null, 1, 0);
+INSERT INTO Users (Id, FullName, Gender, Email, PhoneNumber, isActive, RefreshToken, DateExpireRefreshToken, RoleId, UserName, NormalizedUserName, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount) 
+VALUES ('c3949d5a-7dfd-403a-8627-ddd82eed2be3', 'Red', 0, 'red123@gmail.com', 097873483, 1, null, null, null, 'tutor3', 'TUTOR3', null, 0, 'AQAAAAIAAYagAAAAENQ4q3siLQtY+HUIiz21L/uBReYRyWmt+Gg1t9R/SEGBd03bGmycW1jN+3/l3emU2A==', 'DHRR5Y4KWK5LCULSOOP6EG34JXBKJ5MX', '4a39e4ae-1f12-4bf2-aa53-2eb66b308894', 0, 0, null, 1, 0);
+INSERT INTO Users (Id, FullName, Gender, Email, PhoneNumber, isActive, RefreshToken, DateExpireRefreshToken, RoleId, UserName, NormalizedUserName, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount) 
+VALUES ('2e76159b-8937-46ff-b438-b407dc4b3041', 'Ruce', 0, 'ruce123@gmail.com', 097873483, 1, null, null, null, 'tutor4', 'TUTOR4', null, 0, 'AQAAAAIAAYagAAAAEDS7k87MiVv2UOOzz0jyjNkUzdOYndrvLYgGhmA/YNLYAa7WrpNoJEXurK6oHk/EcA==', '4XFQVZRT36KKNBFUE3AP3KIITRYR5JRN', '347ef032-0efd-447a-ad65-de130176bbd7', 0, 0, null, 1, 0);
+INSERT INTO Users (Id, FullName, Gender, Email, PhoneNumber, isActive, RefreshToken, DateExpireRefreshToken, RoleId, UserName, NormalizedUserName, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount) 
+VALUES ('61fe1135-6e13-4e01-8521-fa817f45a9b5', 'Lisa', 0, 'lisa123@gmail.com', 097873483, 1, null, null, null, 'tutor5', 'TUTOR5', null, 0, 'AQAAAAIAAYagAAAAEDS7k87MiVv2UOOzz0jyjNkUzdOYndrvLYgGhmA/YNLYAa7WrpNoJEXurK6oHk/EcA==', '4XFQVZRT36KKNBFUE3AP3KIITRYR5JRN', '347ef032-0efd-447a-ad65-de130176bbd7', 0, 0, null, 1, 0);
+INSERT INTO Users (Id, FullName, Gender, Email, PhoneNumber, isActive, RefreshToken, DateExpireRefreshToken, RoleId, UserName, NormalizedUserName, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount) 
+VALUES ('a2c8652c-c166-4d2c-a8b4-37898386b6d0', 'Ella', 0, 'ella123@gmail.com', 097873483, 1, null, null, null, 'tutor6', 'TUTOR6', null, 0, 'AQAAAAIAAYagAAAAEOEMOY7h0rJyvF7YpTeOYKPTk53tS0MFqYePVWnWaivBxvpdJT86TkzDTBR/oGMWJw==', 'P3QKHL77AVBTRWV5D6N53UF6GMOUQVL4', 'dd7316ae-c01b-469c-9ffd-22ec0d8b0036', 0, 0, null, 1, 0);
+INSERT INTO Users (Id, FullName, Gender, Email, PhoneNumber, isActive, RefreshToken, DateExpireRefreshToken, RoleId, UserName, NormalizedUserName, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount) 
+VALUES ('7b3c3b5a-974a-4241-9f7b-13e9834a498b', 'Zheng', 1, 'zheng123@gmail.com', 097873483, 1, null, null, null, 'tutor7', 'TUTOR7', null, 0, 'AQAAAAIAAYagAAAAEJLq1t83FJfs6PwLr8q9AauGSQHAlZaUgQAr/xKIBws3e24dbVhHLqDjkU0V9ZvE/g==', 'I6LLTEM7TFXWS3G35JZCNUA3FTVSKK72', '1e29e2c2-65cf-4f48-b25f-b75e4890f093', 0, 0, null, 1, 0);
+INSERT INTO Users (Id, FullName, Gender, Email, PhoneNumber, isActive, RefreshToken, DateExpireRefreshToken, RoleId, UserName, NormalizedUserName, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount) 
+VALUES ('92453ee8-eb79-413e-90dc-3fb08a9a6b16', 'Xin', 1, 'xin123@gmail.com', 097873483, 1, null, null, null, 'tutor8', 'TUTOR8', null, 0, 'AQAAAAIAAYagAAAAEMZIR/KOl9bGXgHZkBZrCCQYp/y2eTAg2heRRZTgNq+a84oe91jAHS0gVae3fa/06g==', '42NMHD3EYSKOYZLW2RI2AELRRTMP3MDU', '62dfff08-d490-4ae6-aa2f-b5819d911f44', 0, 0, null, 1, 0);
+INSERT INTO Users (Id, FullName, Gender, Email, PhoneNumber, isActive, RefreshToken, DateExpireRefreshToken, RoleId, UserName, NormalizedUserName, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount) 
+VALUES ('8eb4c14b-8631-42dc-a3c3-8c3effe9a40f', 'Lieu', 0, 'lieu123@gmail.com', 097873483, 0, null, null, null, 'tutor9', 'TUTOR9', null, 0, 'AQAAAAIAAYagAAAAEFTnSWgwnBW4QF07z45+B5N6aUte2RNzjvOgYxp/W/ScDChzhOM2Addcrr4cn3e7DA==', 'AYLWSKIVZ4YW4I5LFEVHWCKNNR3NBXVV', 'fa38e24a-aa51-4e04-a6a3-8c4880334790', 0, 0, null, 1, 0);
+INSERT INTO Users (Id, FullName, Gender, Email, PhoneNumber, isActive, RefreshToken, DateExpireRefreshToken, RoleId, UserName, NormalizedUserName, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount) 
+VALUES ('41ac2b34-e9c6-472f-8dbd-3b52907c5b85', 'Thanh', 1, 'thanh123@gmail.com', 097873483, 1, null, null, null, 'tutor10', 'TUTOR10', null, 0, 'AQAAAAIAAYagAAAAEMSE7QUgSycSd3E5zGj8zVQEjdTAEsXjZSx8VTOXfF8JMsQdbbIRJXLgxmuIW4HTXA==', 'LQKLFUVRAL3B4ZPVNKKG4OOJTERQV3VG', '37043b49-cb6a-4acb-adad-cb8758da3e20', 0, 0, null, 1, 0);
+
+
+/*INSERT TUTORS*/
+INSERT INTO Tutor (TutorID, Dob, Education, TypeOfDegree, CardID, HourlyRate, Photo, Headline, Description, Address, IsActive, AccountID) 
+VALUES ('T0001', '1998-04-16', 'FPT University', 'College',  077100101, 30, '', 'I am Alice',  
+'My goal is to increase your SAT Math score by identifying your strengths and weaknesses and creating a custom plan tailored for you. I have personally scored an 800/800 on the SAT Math, so I know full well the best methods to prepare and take the exam. My teaching program not only goes over hundreds of practice questions that are identical to the real exam, but also includes a studying plan that to ensure you fully master every math subject that these questions test.', 
+'Error', 1, 'c93968ff-4a02-4dd0-9e91-ca96bf57b694');
+INSERT INTO Tutor (TutorID, Dob, Education, TypeOfDegree, CardID, HourlyRate, Photo, Headline, Description, Address, IsActive, AccountID) 
+VALUES ('T0002', '1998-04-16', 'FPT University', 'Associate Degree',  077100101, 45, '', 'I am John',  
+'I have taught Algebra 2 to the most vulnerable students, those who often didnt have a strong Algebra 1 back ground. I can help find the cracks in your math foundation and propel you to success in this course and the next!', 
+'Error', 0, '33bbad25-0741-4588-b596-6504045778da');
+INSERT INTO Tutor (TutorID, Dob, Education, TypeOfDegree, CardID, HourlyRate, Photo, Headline, Description, Address, IsActive, AccountID) 
+VALUES ('T0003', '1998-04-16', 'FPT University', 'Associate Degree',  077100101, 100, '', 'I am Red',  
+'I have over 10 years of experience helping student raise their SAT Math score by hundreds of points! I cant wait to help you achieve a higher score!', 
+'Error', 1, 'c3949d5a-7dfd-403a-8627-ddd82eed2be3');
+INSERT INTO Tutor (TutorID, Dob, Education, TypeOfDegree, CardID, HourlyRate, Photo, Headline, Description, Address, IsActive, AccountID) 
+VALUES ('T0004', '1998-04-16', 'FPT University', 'Bachelors Degree',  077100101, 150, '', 'I am Ruce',  
+'Is Chemistry lowering your GPA? Do you need a better understanding of complex chemical concepts? I am your tutor! Wherever you are in the country!
+
+I know and teach the subject perfectly. My students have scored as high as 99% on their finals. After each class you will know your concepts and will be able to apply them very quickly to Quizzes, Tests and Finals.', 
+'Error', 1, '2e76159b-8937-46ff-b438-b407dc4b3041');
+INSERT INTO Tutor (TutorID, Dob, Education, TypeOfDegree, CardID, HourlyRate, Photo, Headline, Description, Address, IsActive, AccountID) 
+VALUES ('T0005', '1998-04-16', 'FPT University', 'Masters Degree',  077100101, 60, '', 'I am Lisa',  
+'I have a Degree in Physiology and Chemistry with over seven years of teaching experience. Teaching Certificate in Chemistry for grades 6-12. Over five years of AP Chemistry teaching experience with high pass rates and successful tutoring experiences.', 
+'Error', 1, '61fe1135-6e13-4e01-8521-fa817f45a9b5');
+INSERT INTO Tutor (TutorID, Dob, Education, TypeOfDegree, CardID, HourlyRate, Photo, Headline, Description, Address, IsActive, AccountID) 
+VALUES ('T0006', '1998-04-16', 'FPT University', 'College',  077100101, 45, '', 'I am Ella',  
+'Ive tutored all through college and grad school, mostly in chemistry but also in biology, math and Spanish. I love talking to students from different backgrounds and sharing what makes my subjects fun in a way that makes them more approachable. I am really passionate about science education and communication- I believe we make things a lot harder than they need to be, and I love being a part of detangling a subject for the student.', 
+'Error', 1, 'a2c8652c-c166-4d2c-a8b4-37898386b6d0');
+INSERT INTO Tutor (TutorID, Dob, Education, TypeOfDegree, CardID, HourlyRate, Photo, Headline, Description, Address, IsActive, AccountID) 
+VALUES ('T0007', '1998-04-16', 'FPT University', 'Bachelors Degree',  077100101, 67, '', 'I am Zheng',  
+'I am a current high school chemistry teacher and have been teaching for the past ten years. A little about my background: I graduated from the University of Illinois at Urbana-Champaign with a chemistry degree. The following year, I received my masters degree in education from the University of Illinois at Chicago. During this time I also received my teaching license and have been teaching high school chemistry ever since. ', 
+'Error', 0, '7b3c3b5a-974a-4241-9f7b-13e9834a498b');
+INSERT INTO Tutor (TutorID, Dob, Education, TypeOfDegree, CardID, HourlyRate, Photo, Headline, Description, Address, IsActive, AccountID) 
+VALUES ('T0008', '1998-04-16', 'FPT University', 'Bachelors Degree',  077100101, 75, '', 'I am Xin',  
+'The thing I love most about teaching chemistry are those "Aha!" moments and seeing my students sense of accomplishment shine through after learning a difficult concept. My approach in the classroom mirrors my tutoring methods in that I believe in a problem-based approach. Practice problems are the key to success in chemistry and therefore I always have a whiteboard nearby.', 
+'Error', 0, '92453ee8-eb79-413e-90dc-3fb08a9a6b16');
+INSERT INTO Tutor (TutorID, Dob, Education, TypeOfDegree, CardID, HourlyRate, Photo, Headline, Description, Address, IsActive, AccountID) 
+VALUES ('T0009', '1998-04-16', 'FPT University', 'Doctoral Degree',  077100101, 80, '', 'I am Lieu',  
+'Chemistry is a notoriously tough subject but practice truly can make these concepts easy. Feel free to contact me if there is anything else you would like to know or to schedule a lesson. Looking forward to working with you.', 
+'Error', 1, '8eb4c14b-8631-42dc-a3c3-8c3effe9a40f');
+INSERT INTO Tutor (TutorID, Dob, Education, TypeOfDegree, CardID, HourlyRate, Photo, Headline, Description, Address, IsActive, AccountID) 
+VALUES ('T0010', '1998-04-16', 'FPT University', 'Associate Degree',  077100101, 90, '', 'I am Thanh',  
+'I CAN HELP YOU OR YOUR CHILD GET A 4 or 5 on the AP exams I tutor. I have worked with many students and those that use me regularly do very well on their exams!!!! This is my 8th year on Wyzant as a tutor and I have been very successful each year. WORK WITH ME ONLINE OR IN PERSON. I can tutor both ways and my online sessions are just like my in person sessions!!!', 
+'Error', 1, '41ac2b34-e9c6-472f-8dbd-3b52907c5b85');
+
+
+/*INSERT GRADES*/
+INSERT INTO Grade (GradeID, Number) VALUES ('G0001', 1);
+INSERT INTO Grade (GradeID, Number) VALUES ('G0002', 2);
+INSERT INTO Grade (GradeID, Number) VALUES ('G0003', 3);
+INSERT INTO Grade (GradeID, Number) VALUES ('G0004', 4);
+INSERT INTO Grade (GradeID, Number) VALUES ('G0005', 5);
+INSERT INTO Grade (GradeID, Number) VALUES ('G0006', 6);
+INSERT INTO Grade (GradeID, Number) VALUES ('G0007', 7);
+INSERT INTO Grade (GradeID, Number) VALUES ('G0008', 8);
+INSERT INTO Grade (GradeID, Number) VALUES ('G0009', 9);
+INSERT INTO Grade (GradeID, Number) VALUES ('G0010', 10);
+INSERT INTO Grade (GradeID, Number) VALUES ('G0011', 11);
+INSERT INTO Grade (GradeID, Number) VALUES ('G0012', 12);
+
+
+/*INSERT SUBJECT GROUPS*/
+INSERT INTO SubjectGroup (SubjectGroupID, SubjectName, Description) VALUES ('S0001', 'Math', ' ');
+INSERT INTO SubjectGroup (SubjectGroupID, SubjectName, Description) VALUES ('S0002', 'Physical', ' ');
+INSERT INTO SubjectGroup (SubjectGroupID, SubjectName, Description) VALUES ('S0003', 'Chemistry', ' ');
+INSERT INTO SubjectGroup (SubjectGroupID, SubjectName, Description) VALUES ('S0004', 'English', ' ');
+INSERT INTO SubjectGroup (SubjectGroupID, SubjectName, Description) VALUES ('S0005', 'Lecturer', ' ');
+INSERT INTO SubjectGroup (SubjectGroupID, SubjectName, Description) VALUES ('S0006', 'History', ' ');
+INSERT INTO SubjectGroup (SubjectGroupID, SubjectName, Description) VALUES ('S0007', 'Geography ', ' ');
+INSERT INTO SubjectGroup (SubjectGroupID, SubjectName, Description) VALUES ('S0008', 'Biology', ' ');
+INSERT INTO SubjectGroup (SubjectGroupID, SubjectName, Description) VALUES ('S0009', 'Informatics', ' ');
+INSERT INTO SubjectGroup (SubjectGroupID, SubjectName, Description) VALUES ('S0010', 'Japanese', ' ');
+
+
+/*INSERT SUBJECT*/
+INSERT INTO Subject (SubjectID, GradeID, SubjectGroupID) VALUES ('MAT01', 'G0001', 'S0001');
+INSERT INTO Subject (SubjectID, GradeID, SubjectGroupID) VALUES ('MAT11', 'G0011', 'S0001');
+INSERT INTO Subject (SubjectID, GradeID, SubjectGroupID) VALUES ('CHE12', 'G0012', 'S0003');
+INSERT INTO Subject (SubjectID, GradeID, SubjectGroupID) VALUES ('HIS08', 'G0008', 'S0006');
+INSERT INTO Subject (SubjectID, GradeID, SubjectGroupID) VALUES ('ENG03', 'G0003', 'S0004');
+INSERT INTO Subject (SubjectID, GradeID, SubjectGroupID) VALUES ('ENG09', 'G0009', 'S0004');
+INSERT INTO Subject (SubjectID, GradeID, SubjectGroupID) VALUES ('MAT12', 'G0012', 'S0001');
+INSERT INTO Subject (SubjectID, GradeID, SubjectGroupID) VALUES ('MAT09', 'G0009', 'S0001');
+INSERT INTO Subject (SubjectID, GradeID, SubjectGroupID) VALUES ('MAT08', 'G0008', 'S0001');
+INSERT INTO Subject (SubjectID, GradeID, SubjectGroupID) VALUES ('MAT07', 'G0007', 'S0001');
+
+
+/*INSERT CLASS*/
+INSERT INTO Class (ClassID, Quantity, Price, Description, Status, TutorID, StudentID, SubjectID) 
+VALUES ('C0001', 1, 65, 'Mat 12', 'done', 'T0010', 'S0001', 'MAT12');
+INSERT INTO Class (ClassID, Quantity, Price, Description, Status, TutorID, StudentID, SubjectID) 
+VALUES ('C0002', 1, 65, 'Che 12', 'done', 'T0010', 'S0004', 'CHE12');
+INSERT INTO Class (ClassID, Quantity, Price, Description, Status, TutorID, StudentID, SubjectID) 
+VALUES ('C0003', 1, 65, 'Mat 12', 'done', 'T0010', 'S0006', 'MAT12');
+INSERT INTO Class (ClassID, Quantity, Price, Description, Status, TutorID, StudentID, SubjectID) 
+VALUES ('C0004', 1, 65, 'Eng 9', 'done', 'T0009', 'S0003', 'ENG09');
+INSERT INTO Class (ClassID, Quantity, Price, Description, Status, TutorID, StudentID, SubjectID) 
+VALUES ('C0005', 1, 65, 'Eng 3', 'done', 'T0009', 'S0005', 'ENG03');
+
+
+/*INSERT FEEDBACK*/
+INSERT INTO Feedback (FeedbackID, CreateDay, Description, Rate, IsActive, StudentID, TutorID, ClassID) 
+VALUES ('F0001', '2024-04-16', 
+'Thanh works with both my teenage children individually on a weekly basis. Each has specific math needs including SAT prep and Algebra 2 honors. He is able to repeatedly deliver consistent, professional lesson plans. Keith is more than knowledgeable and has the ability to tailor each lesson so there is an increased math base each week. He is very dependable. We are all more than happy we found him!', 
+4, 1, 'S0001', 'T0010', 'C0001');
+INSERT INTO Feedback (FeedbackID, CreateDay, Description, Rate, IsActive, StudentID, TutorID, ClassID) 
+VALUES ('F0002', '2024-04-16', 
+'Thanh was patient and helpful in showing me how to navigate through commercial floor plans. Really great demeanor. I look forward to working with him again. Thanks so much!',
+3, 1, 'S0004', 'T0010', 'C0002');
+INSERT INTO Feedback (FeedbackID, CreateDay, Description, Rate, IsActive, StudentID, TutorID, ClassID)  
+VALUES ('F0003', '2024-04-16', 
+'Great communicator, and accessible. My child has met with Keith several times. He’s patient and meticulous about breaking down the steps needed to gain understanding of concepts. My child tests have been going up by one grade point since these sessions began.', 
+5, 1, 'S0006', 'T0010', 'C0003');
+INSERT INTO Feedback (FeedbackID, CreateDay, Description, Rate, IsActive, StudentID, TutorID, ClassID) 
+VALUES ('F0004', '2024-04-16', 
+'Not good tutor!!!',
+2, 1, 'S0003', 'T0009', 'C0004');
+INSERT INTO Feedback (FeedbackID, CreateDay, Description, Rate, IsActive, StudentID, TutorID, ClassID) 
+VALUES ('F0005', '2024-04-16', 
+'Since my son has been meeting with Lieu, his Math grades have improved and he is not afraid of upcoming tests. Lieu does a good job of explaining concepts and helping my son learn.', 
+5, 1, 'S0005', 'T0009', 'C0005');
